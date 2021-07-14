806b4362c57bb835413ca41f395b8cfccdbe6d0a
Niels de Water <nielsdw@gmail.com>
2021-07-14 14:48:47 +0200

    tests

--- tests/test_get_shortcode.py
+++ tests/test_get_shortcode.py
@@ -2,14 +2,14 @@ from app.app import ShortCode, app, db
 db.create_all()
 
 
-def test_get_stats():
+def test_get_shortcode():
     short_code = ShortCode(short_code='xxxxxx', url='https://www.google.com')
     db.session.add(short_code)
     db.session.commit()
     with app.test_client() as test_client:
-        response = test_client.get('/xxxxxx/stats')
-        assert response.status_code == 200
-        assert response.json["created"] is not None
+        response = test_client.get('/xxxxxx')
+        assert response.status_code == 302
+        assert "https://www.google.com" == response.headers["Location"]
         response = test_client.get('/yyyyyy')
         assert response.status_code == 404
 
